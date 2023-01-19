bdb452ba66167757e36c59e826be02fd03dd8909
LucasVianaSampaio <lv6612530@gmail.com>
2023-01-19 20:53:59 -0300

    script

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 const form = document.querySelector('#form-habits')
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
 
-const data = {
-  run: ['01-01', '01-02', '01-06', '01-08', '01-09'],
-  water: ['01-02', '01-05'],
-  muscle: ['01-03', '01-04']
+button.addEventListener('click', add)
+form.addEventListener('change', save)
+
+function add() {
+  const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
+
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert('Dia já incluso❌')
+    return
+  }
+
+  alert('Dia adicionado com sucesso✔')
+  nlwSetup.addDay(today)
 }
+
+function save() {
+  localStorage.setItem('NLWSetupHabits', JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem('NLWSetupHabits')) || {}
 
 nlwSetup.setData(data)
 nlwSetup.load()
